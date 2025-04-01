2775e36324a096635c9a902198081339f1089ec8
Pe-drou <phsr300408@gmail.com>
2025-04-01 16:30:28 -0300

    login + perfil adm

---
+++ perfil_adm.php
@@ -0,0 +1,118 @@
+<?php
+// Você não irá escapar do backend
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home (Admin) - Loca dos veículos</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- bootstrap icones -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body class="container py-4">
+    <div class="container py-4">
+        <!-- Barra de informações de usuário (adm) -->
+        <div class="row mb-4">
+            <div class="col-md-12">
+                <div class="d-flex justify-content-between align-items-center inicio">
+                    <h1>Sistema de locadora de veículos</h1>
+                    <div class="d-flex align-items-center gap-3 user-info mx-3">
+                        <span class="user-icon">
+                            <i class="bi bi-person-circle" style="font-size:25px"></i>
+                        </span>
+                        <!-- Bem vindom [usuário] -->
+                        <span class="welcome-text">
+                            Bem-vindo, <strong>Admin</strong>!
+                        </span>
+                        <!-- bomtão di logaut -->
+                        <a href="login.php" class="btn btn-outline-danger d-flex align-items-center gap-1"><i class="bi bi-box-arrow-right"></i> Sair</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Formulário para adicionar novos veículos -->
+        <div class="row same-height-row">
+            <div class="col-md-6">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h4 class="mb-0">Adicionar novos veículos</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="post" class="needs-validation" novalidate>
+                            <div class="mb-3">
+                                <label for="modelo" class="form-label">Modelo:</label>
+                                <input type="text" name="modelo" id="modelo" class="form-control">
+                                <div class="invalid-feedback">
+                                    Informe um modelo válido!
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="placa" class="form-label">Placa:</label>
+                                <input type="text" name="placa" id="placa" class="form-control">
+                                <div class="invalid-feedback">
+                                    Informe uma placa válido!
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="tipo" class="form-label">Tipo:</label>
+                                <select name="tipo" id="tipo" class="form-select" required>
+                                    <option value="carro">Carro</option>
+                                    <option value="moto">Moto</option>
+                                    <option value="caminhao">Caminhão</option>
+                                    <option value="maritimo">Barco</option>
+                                    <option value="subaquatico">Submarino</option>
+                                    <option value="aereo">Aeronave</option>
+                                    <option value="animal">Animal</option>
+                                    <option value="nave">Nave Espacial</option>
+                                    <option value="mistico">Criatura Mística</option>
+                                    <option value="tecnologico">Dispositivo Portátil de Deslocamento</option>
+                                    <option value="null" disabled selected hidden></option>
+                                </select>
+                            </div>
+                            <button class="btn btn-outline-success w-100" type="submit" name="adicionar">
+                                Adicionar veículo
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Calculadora de aluguel -->
+            <div class="col-md-6">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h4 class="mb-0">Calcular a previsão de aluguel</h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="post" class="needs-validation" novalidate>
+                            <div class="mb-3">
+                                <label for="tipo" class="input-label">Tipo:</label>
+                                <select name="tipo" id="tipo" class="form-select" required>
+                                    <option value="carro">Carro</option>
+                                    <option value="moto">Moto</option>
+                                    <option value="caminhao">Caminhão</option>
+                                    <option value="maritimo">Barco</option>
+                                    <option value="subaquatico">Submarino</option>
+                                    <option value="aereo">Aeronave</option>
+                                    <option value="animal">Animal</option>
+                                    <option value="nave">Nave Espacial</option>
+                                    <option value="mistico">Criatura Mística</option>
+                                    <option value="tecnologico">Dispositivo Portátil de Deslocamento</option>
+                                    <option value="null" disabled selected hidden></option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label for="dias" class="input-label">Tempo em dias: </label>
+                                <input type="number" name="dias" id="tempo" class="form-control" value="1" required>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
